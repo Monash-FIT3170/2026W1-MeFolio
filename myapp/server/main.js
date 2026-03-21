e35b3b3141f3b4e56cf4173f127f9e679a558bce
jleu0009 <jleu0009@student.monash.edu>
2026-03-21 11:27:22 +1100

    Step 2 - Publish collection and define methods

--- myapp/server/main.js
+++ myapp/server/main.js
@@ -1,54 +1,23 @@
 import { Meteor } from "meteor/meteor";
 import { LinksCollection } from "/imports/api/links";
 import { Random } from "meteor/random";
+import { TaskCollection } from "/imports/api/tasks"; 
 
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
-}
+// A publication that allows the client to subscribe to the data/collection of tasks. 
+// It returns all tasks sorted by creation date in descending order.
+Meteor.publish('tasks.all', function(){
+  return TaskCollection.find({}, {sort: {createdAt: -1}});
+})
 
-Meteor.startup(async () => {
-  // If the Links collection is empty, add some data.
-  if ((await LinksCollection.find().countAsync()) === 0) {
-    await insertLink({
-      title: "Do the Tutorial",
-      url: "https://docs.meteor.com/tutorials/react/",
-    });
-
-    await insertLink({
-      title: "Follow the Guide",
-      url: "https://docs.meteor.com/tutorials/application-structure/",
-    });
-
-    await insertLink({
-      title: "Read the Docs",
-      url: "https://docs.meteor.com",
-    });
-
-    await insertLink({
-      title: "Discussions",
-      url: "https://forums.meteor.com",
-    });
-
-    await insertLink({
-      title: "Join us on Discord",
-      url: "https://discord.gg/6mS3wHNg",
-    });
-
-    await insertLink({
-      title: "Deploying in Galaxy",
-      url: "https://www.meteor.com/hosting",
-    });
-  }
-
-  // We publish the entire Links collection to all clients.
-  // In order to be fetched in real-time to the clients
-  Meteor.publish("links", function () {
-    return LinksCollection.find();
-  });
-});
-
+// Meteor methods are used to perform database operations, and can add various methods inside the object passed to Meteor.methods(). 
 Meteor.methods({
-  about() {
-    return `This is a Meteor application running React with React Router. this is a generated id: ${Random.id()}`;
+  async 'tasks.insert'(text) {
+    return await TaskCollection.insertAsync({
+      text:text,
+      createdAt: new Date(),
+    });
   },
+  async 'tasks.remove'(taskId) {
+    return await TaskCollection.removeAsync(taskId);
+  }
 });
