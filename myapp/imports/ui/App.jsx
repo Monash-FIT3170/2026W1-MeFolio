8eb9e49461520355f42837d4da3858b5cbc6b0c3
jleu0009 <jleu0009@student.monash.edu>
2026-03-21 11:29:26 +1100

    Step 4 - Link new component to main app (see also main.jsx)

--- myapp/imports/ui/App.jsx
+++ myapp/imports/ui/App.jsx
@@ -1,13 +1,11 @@
-import { Counter } from "./Counter.jsx";
 import { Header } from "./Header.jsx";
-import { Info } from "./Info.jsx";
+import { Tasks } from "./Tasks.jsx";
 
 export const App = () => (
   <div className="page">
     <Header />
     <main className="main">
-      <Counter />
-      <Info />
+      <Tasks />
     </main>
   </div>
 );
