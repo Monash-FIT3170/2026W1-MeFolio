abd4b97fea57062f08ef5171bbda655e010c59e8
showie0032 <show0032@student.monash.edu>
2026-04-24 17:12:59 +1000

    FEAT-01: UI Design into App implementation, Tailwind Install

--- mefolio/imports/ui/App.jsx
+++ mefolio/imports/ui/App.jsx
@@ -1,7 +1,32 @@
+import React, { useState } from 'react';
+import { LoginPage } from "./LoginPage.jsx";
+import { SignUpPage } from "./SignUpPage.jsx";
 import { PortfolioBuilderView } from "./PortofolioBuilderView.jsx";
 
-export const App = () => (
-  <div className="page">
-    <PortfolioBuilderView />
-  </div>
-);
+export const App = () => {
+  const [view, setView] = useState('signin');
+
+  if (view === 'signin') {
+    return (
+      <LoginPage 
+        onSignIn={() => setView('main')} 
+        onSwitchToSignUp={() => setView('signup')} 
+      />
+    );
+  }
+
+  if (view === 'signup') {
+    return (
+      <SignUpPage 
+        onSignUp={() => setView('main')} 
+        onSwitchToSignIn={() => setView('signin')} 
+      />
+    );
+  }
+
+  return (
+    <div className="page">
+      <PortfolioBuilderView />
+    </div>
+  );
+};
