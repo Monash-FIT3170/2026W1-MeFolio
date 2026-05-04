35151c7c5d9b562ed7480cb23dfeec1c72bcb606
mcha0202 <marcus.chanwh@gmail.com>
2026-05-04 11:27:20 +1000

    FEAT-02: Mode button
    - more or less working using react-router
    - just need to fix styling with tailwind

--- mefolio/imports/ui/App.jsx
+++ mefolio/imports/ui/App.jsx
@@ -1,7 +1,10 @@
 import { PortfolioBuilderView } from "./PortofolioBuilderView.jsx";
+import { BrowserRouter as Router} from "react-router-dom";
 
 export const App = () => (
+  <Router>
   <div className="page">
     <PortfolioBuilderView />
   </div>
+  </Router>
 );
