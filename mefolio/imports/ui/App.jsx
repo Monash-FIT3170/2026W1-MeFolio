c02ee364f625da71202504dc30121f01708b04c7
Joyce Leung <jyc.leung02@gmail.com>
2026-05-04 15:11:49 +1000

    Revert "Feat 01  user authentication  UI design"

--- mefolio/imports/ui/App.jsx
+++ mefolio/imports/ui/App.jsx
@@ -1,62 +1,7 @@
-import React, { useState } from 'react';
-import { useTracker } from 'meteor/react-meteor-data';
-import { Meteor } from 'meteor/meteor';
-import { LoginPage } from "./LoginPage.jsx";
-import { SignUpPage } from "./SignUpPage.jsx";
-import { ForgotPasswordPage } from "./ForgotPasswordPage.jsx"; 
-import { TermsOfServicePage } from "./TermsOfServicePage.jsx";
-import { PrivacyPolicyPage } from "./PrivacyPolicyPage.jsx";
 import { PortfolioBuilderView } from "./PortofolioBuilderView.jsx";
 
-export const App = () => {
-  const [view, setView] = useState('signin');
-
-  const userId = useTracker(() => Meteor.userId());
-  const isLoggingIn = useTracker(() => Meteor.loggingIn());
-
-  if (isLoggingIn) return null; 
-
-  if (userId) {
-    return (
-      <div className="page">
-        <PortfolioBuilderView />
-      </div>
-    );
-  }
-
-  if (view === 'signin') {
-    return (
-      <LoginPage 
-        onSignIn={() => {}} 
-        onSwitchToSignUp={() => setView('signup')} 
-        onForgotPassword={() => setView('forgot')}
-      />
-    );
-  }
-
-  if (view === 'forgot') {
-    return (
-      <ForgotPasswordPage 
-        onBackToLogin={() => setView('signin')} 
-        onPasswordReset={() => setView('signin')} 
-      />
-    );
-  }
-
-  if (view === 'terms') {
-    return <TermsOfServicePage onBack={() => setView('signup')} />;
-  }
-
-  if (view === 'privacy') {
-    return <PrivacyPolicyPage onBack={() => setView('signup')} />;
-  }
-
-  return (
-    <SignUpPage 
-      onSignUp={() => {}} 
-      onSwitchToSignIn={() => setView('signin')} 
-      onShowTerms={() => setView('terms')}
-      onShowPrivacy={() => setView('privacy')}
-    />
-  );
-};
+export const App = () => (
+  <div className="page">
+    <PortfolioBuilderView />
+  </div>
+);
