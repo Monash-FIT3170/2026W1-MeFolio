a07ee661d1c4bd2198ae4e2c672aa71cd812c2c8
mcha0202 <marcus.chanwh@gmail.com>
2026-05-04 14:32:36 +1000

    Revert "FEAT-02: Mode button"
    This reverts commit 35151c7c5d9b562ed7480cb23dfeec1c72bcb606.

--- mefolio/imports/ui/App.jsx
+++ mefolio/imports/ui/App.jsx
@@ -1,10 +1,7 @@
 import { PortfolioBuilderView } from "./PortofolioBuilderView.jsx";
-import { BrowserRouter as Router} from "react-router-dom";
 
 export const App = () => (
-  <Router>
   <div className="page">
     <PortfolioBuilderView />
   </div>
-  </Router>
 );
--- mefolio/imports/ui/ModeButton.jsx
+++ mefolio/imports/ui/ModeButton.jsx
@@ -1,31 +1,23 @@
 import { useState } from "react";
 
-/**
- * Mode switch button component to toggle between builder and preview modes.
- * 
- * @param {boolean} initialPreview - Whether in preview mode or not.
- * @param {function} onToggle - Callback function when the mode is toggled.
- * @returns Button element that toggles between builder and preview modes.
- */
-export const ModeSwitch = ({ initialPreview = false, onToggle}) => {
-  const [preview, setPreview] = useState(initialPreview); // state to track if in preview mode or not
+export const ModeSwitch = ({ inititalPreview = false}, onClick) => {
+  const [preview, setPreview] = useState(inititalPreview); // state to track if in preview mode or not
 
-  const handleClick = () => { // when clicking mode switch
-    const next = !preview
-    setPreview(next);
+  const click = () => { // when clicking mode switch
+    setPreview(!preview);
     if (onToggle) {
-        onToggle(next);
+        onToggle(!preview);
     }
   }
 
   return (
     <button
-      onClick={handleClick} //TODO double check styling with tailwind
+      onClick={handleClick}
       className={`
         flex items-center gap-2 px-5 py-2.5 rounded-full
         border transition-all duration-200
         ${
-          preview
+          active
             ? "bg-indigo-100 border-indigo-300 text-indigo-600"
             : "bg-gray-100 border-gray-200 text-indigo-500 hover:bg-gray-200"
         }
