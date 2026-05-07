1ede87ef022d96567ea3231d40de5016e6326269
Jas <129500148+JkGiThuB821@users.noreply.github.com>
2026-05-08 01:16:51 +1000

    Added the base Portfolio View Section

--- mefolio/imports/ui/PortfolioView.jsx
+++ mefolio/imports/ui/PortfolioView.jsx
@@ -1,4 +1,33 @@
-export const PortfolioView  = () => { 
-    return (
-        <div>Sample Portfolio</div>
-    )};
+import { PortfolioCollection } from "../api/portfolio";
+
+import { useTracker } from "meteor/react-meteor-data";
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+export const PortfolioView = () => {
+  const { portfolios, isLoading } = useTracker(() => {
+    const handler = Meteor.subscribe("portfolios.all");
+    return {
+      portfolios: PortfolioCollection.find({}).fetch(),
+      isLoading: !handler.ready()
+    };
+  });
+
+  const addPortfolio = () => {
+    Meteor.call("portfolios.insert", portfolioData);
+    setPortfolioData({}); 
+  };
+  const removePortfolio = (id) => {
+    Meteor.call("portfolios.remove", id);
+  };
+  return (
+    <div>
+      <Navbar />
+      <About />
+      <section>
+        <h2>Portfolios</h2>
+        <div>Placeholder for portfolio UI</div>
+      </section>
+    </div>
+  ); //TODO: Create UI to edit portfolio details instead of returning none.
+};
