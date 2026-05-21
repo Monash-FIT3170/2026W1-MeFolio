046c5ad6fcd6f8787193dcc46252921ded925d30
showie0032 <show0032@student.monash.edu>
2026-05-21 16:21:06 +1000

    FEAT-09: Formatting Fix - Import clean up, restoring file and formatting

--- mefolio/client/main.jsx
+++ mefolio/client/main.jsx
@@ -3,17 +3,17 @@ import { Meteor } from "meteor/meteor";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "/imports/ui/App";
 import "/imports/ui/styles.css";
-import { ResponsiveProvider } from '/imports/ui/contexts/ResponsiveContext';
-import './main.css';
+import { ResponsiveProvider } from "/imports/ui/Contexts/ResponsiveContext";
+import "./main.css";
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container);
   root.render(
     <BrowserRouter>
-    <ResponsiveProvider>
-      <App />
-    </ResponsiveProvider>
+      <ResponsiveProvider>
+        <App />
+      </ResponsiveProvider>
     </BrowserRouter>,
   );
 });
